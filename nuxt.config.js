3f23e703d8ff896447a3d9c3665eb73efd226447
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-05-04 19:36:26 -0500

    Cambiando direccion api

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: development ? ' http://localhost:3001/api' : 'https://amazon-clone-api.now.sh/api'
+    baseURL: development ? ' http://localhost:3001/api' : 'https://morito-store-api.now.sh/api'
   },
   /*
   ** Build configuration
